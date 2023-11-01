7f8b986543c072c3dd86602640104fb09166b326
bebus <v.gerashchenkoua@gmail.com>
2023-11-01 16:21:11 +0200

    added download logiс

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,6 +1,18 @@
+import asyncio
+
 from loader import app
+from pyrogram.types import Message
 from pyrogram import filters
 
+from utils.downloader import download_video
+
+
+async def check_for_url(url):
+    ...
+
 @app.on_message(filters.private)
-async def hello(client, message):
-    await message.reply("Hello from Pyrogram!")
+async def hello(client, message:Message):
+    msg_to_edit=await message.reply("Download started!")
+    out_path,audio_name=await download_video(message.text,message.from_user.id,msg_to_edit)
+    audio=open(out_path,'rb')
+    await message.reply_audio(audio,title=audio_name)
---
+++ utils/downloader.py
@@ -0,0 +1,61 @@
+import asyncio
+import os.path
+import time
+
+from pyrogram.types import Message
+import yt_dlp
+
+
+async def edit_message_progress(msg: Message, progress: str):
+    print("amogus")
+    await msg.edit(f'Downloading video...\n{progress}')
+
+async def download_video(url:str,user_id:str|int,msg_to_edit:Message):
+    progress:float=0.0
+    last_edit_time = 0
+    def my_hook(d):
+        nonlocal last_edit_time
+        if d['status'] == 'finished':
+            file_tuple = os.path.split(os.path.abspath(d['filename']))
+            print("Done downloading {}".format(file_tuple[1]))
+        if d['status'] == 'downloading':
+            p = d['_percent_str']
+            p = p.replace('%', '')
+            progress=str(p)
+
+            current_time = time.time()
+            if current_time - last_edit_time >= 1.0:
+                asyncio.create_task(edit_message_progress(msg_to_edit, f'{progress}%'))
+                last_edit_time = current_time
+                print(d['filename'], d['_percent_str'], d['_eta_str'])
+
+    temp_path = os.path.abspath('utils/temp')
+    print(temp_path)
+    if not os.path.isdir(temp_path+f'/{user_id}'):
+        os.mkdir(f'{temp_path}/{user_id}')
+
+    with yt_dlp.YoutubeDL() as ydl:
+        info_dict=ydl.extract_info(url, download=False)
+        video_url = info_dict.get("url", None)
+        video_id = info_dict.get("id", None)
+        video_title = info_dict.get('title', None)
+
+    out_path=f'{temp_path}/{user_id}/{video_title}'
+    ydl_opts = {
+        'format': 'm4a/bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '256'
+        }],
+        'postprocessor_args': [ '-vn', '-ar', '44100', '-ac', '2','-b:a','256k','-metadata',f'title={video_title}'],
+        'outtmpl': f'{out_path}',
+        'quiet': 'False',
+        'noprogress': 'False',
+        'progress_hooks':[my_hook]
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    return out_path+".mp3",video_title
